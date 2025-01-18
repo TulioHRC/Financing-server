959106782a7ee4fd2b8793aab2c90e5f15d44664
TulioHRC <grove1411@gmail.com>
2025-01-18 12:35:21 -0300

    feat: add sort on investiments return

--- src/investiments/investiments.service.ts
+++ src/investiments/investiments.service.ts
@@ -17,7 +17,11 @@ export class InvestimentsService {
   }
 
   async findAll() {
-    return this.prisma.investiments.findMany();
+    return (await this.prisma.investiments.findMany()).sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
   }
 
   async findOne(id: string) {
